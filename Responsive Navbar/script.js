47896a54d2c117cb6f7e8ba3e10d6cd763f09d3c
Bogdan Nitica <bogdan.nitica99@gmail.com>
2021-03-31 23:25:48 +0300

    add responsive navbar

---
+++ Responsive Navbar/script.js
@@ -0,0 +1,11 @@
+const navSlide = () => {
+  console.log("Alo");
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+};
+
+navSlide();
